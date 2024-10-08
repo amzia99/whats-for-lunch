3135a5cc71268daccf17147473de6a6b92d6cb00
Abdullah Zia <amz1cflp99@gmail.com>
2024-10-08 17:35:18 -0400

    Completed What should I do for lunch assignment

--- lunch.js
+++ lunch.js
@@ -14,8 +14,18 @@
  */
 
 const whatToDoForLunch = function(hungry, availableTime) {
-  console.log("I don't know what to do!");
-}
+    if (!hungry) {
+    console.log("Wait until you're hungry.");
+  } else {
+    if (availableTime < 20) {
+      console.log("Pick up a snack or grab something you have ready at home.");
+    } else if (availableTime >= 20 && availableTime <= 30) {
+      console.log("You deserve a break and should take time to cook a tasty meal.");
+    } else {
+      console.log("This is an intense program after all, you should probably reconsider.");
+    }
+    }
+    }
 
 
 /*
